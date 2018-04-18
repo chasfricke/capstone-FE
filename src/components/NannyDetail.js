e48a066d82c50bf8bcef33414c8d703cf90cfaba
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-18 10:18:09 -0600

    nodemailer test email working

--- src/components/NannyDetail.js
+++ src/components/NannyDetail.js
@@ -7,6 +7,9 @@ export class NannyDetail extends React.Component {
 	constructor(props) {
 		super(props)    
 		this.state = null;
+
+		this.handleChange = this.handleChange.bind(this)
+    	this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
 	componentDidMount() {
@@ -15,44 +18,67 @@ export class NannyDetail extends React.Component {
 		this.getNanny(id)
 	}
 	
-	  getNanny = (id) => {
+	getNanny = (id) => {
 		return fetch(`https://gnarly-nannies.herokuapp.com/nanny_account_info/${id}`)
 		  .then(response => response.json())
 		  .then(data => {
-			this.setState(data.nanny_account_info)})
-		}
+			// this.setState(data.nanny_account_info)})
+			this.setState({nannyData: data.nanny_account_info})})
+			// this.setState({nannyData: nannyData})
+	}
 
-		renderSports = () => {
-			const sports = ['surfing', 'kayaking', 'rock_climbing', 'skateboarding', 'hiking', 'mountain_biking', 'skiing', 'snowboarding']		
-			const results = sports.filter(sport => this.state[sport] === true) 
-			// const sportsLabels = ['Surfing', 'Kayaking', 'Rock Climbing', 'Skateboarding', 'Hiking', 'Mountain Biking', 'Skiing', 'Snowboarding'];
-			console.log (results)
-			const resultListItem = results.map(result => <li className="check-skill-container"><img className="check" src={check} alt="check"/>{result}</li>)
-			return (
-				<ul>
-					{resultListItem}
-				</ul>
-			)	
-		}
+	renderSports = () => {
+		const sports = ['surfing', 'kayaking', 'rock_climbing', 'skateboarding', 'hiking', 'mountain_biking', 'skiing', 'snowboarding']		
+		const results = sports.filter(sport => this.state.nannyData[sport] === true) 
+		const resultListItem = results.map(result => <li key={result} className="check-skill-container"><img className="check" src={check} alt="check"/>{result}</li>)
+		return (
+			<ul>
+				{resultListItem}
+			</ul>
+		)	
+	}
+
+	handleChange = e => {
+		this.setState({[e.target.name]: e.target.value})
+	}
+
+	//Nodemailer Post 
+
+	handleSubmit(e) {
+		e.preventDefault()
+		console.log("submit")
+		console.log(this.state)
+
+		fetch('http://localhost:8080/api/form', {
+			method: 'POST',
+			body: JSON.stringify(this.state),
+			headers: new Headers({
+				'Content-Type': 'application/json'
+			})
+		})
+			.then(response => response.json())
+			.catch(error => console.error('Error:', error))
+	}
 
 	render() {
 		if (this.state === null) {
 			return <div>Loading...</div>
 		}
 		return (
+			<div>
 			<div className="detail-container">
 				<Link to="/locations/denver" >
 					<button className="back-button">Back</button>
 				</Link>
 				<div className="nanny-detail-main">
-					<img className="nanny-detail-img" src={this.state.photo_url} alt="profile img"/>
+					<img className="nanny-detail-img" src={this.state.nannyData.photo_url} alt="profile img"/>
 					<div className="basic-info-container">
 						<div className="basic-info">
 							<div>
-								<h4>{this.state.first_name + " " + this.state.last_name}</h4>
-								<p>{this.state.city}, {this.state.state}</p>
-								<p>Hourly Rate: ${this.state.hourly_rate}</p>
-								<p>Experience: {this.state.years_experience} years</p>
+								<h4>{this.state.nannyData.first_name + " " + this.state.nannyData.last_name}</h4>
+								<p>{this.state.nannyData.city}, {this.state.nannyData.state}</p>
+								<p>Hourly Rate: ${this.state.nannyData.hourly_rate}</p>
+								<p>Experience: {this.state.nannyData.years_experience} years</p>
 							</div>
 							<div className="skills-list">
 								<h4>Trips Offered</h4>
@@ -63,15 +89,45 @@ export class NannyDetail extends React.Component {
 						</div>
 						<div className="background">
 							<h4>About Me</h4>
-							<p>{this.state.background}</p>
+							<p>{this.state.nannyData.background}</p>
 						</div>
-						
 					</div>
-					
-					
 				</div>
-				
 			</div>
+			
+			<form onSubmit={this.handleSubmit} className="nanny-email-form">
+			<h4>Email {this.state.nannyData.first_name}</h4>
+				<div className = "nanny-email-input-container">
+					<label>Your Name</label>
+					<input
+						type="text"
+						name="user_name"
+						onChange={this.handleChange} />
+				</div>
+				<div className = "nanny-email-input-container">
+					<label>Your Email</label>
+					<input
+						type="email"
+						name="user_email"
+						onChange={this.handleChange} />
+				</div>
+				<div className = "nanny-email-input-container">
+					<label>Subject Line</label>
+					<input
+						type="text"
+						name="user_subject"
+						onChange={this.handleChange} />
+				</div>
+				<div className = "nanny-email-input-container">
+					<label>Message</label>
+					<textarea
+						name="user_message"
+						onChange={this.handleChange} />
+				</div>
+				<button className="email-submit-button">Send</button>
+			</form>
+			</div>
+			
 		)
 	}
 }
