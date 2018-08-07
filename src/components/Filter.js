4b2bea30ae06bdf30a8fa4fe00ed0ea6a195b125
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-08-06 22:57:43 -0600

    switched filter bar from left to top of the screen

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -25,81 +25,79 @@ export class Filter extends React.Component {
 	render() {
 
 		return (
-			<div className="filter-title-nannies-container">
+			<div>
 				<h3 className="nannies-title">DENVER NANNIES</h3>
-			<div className="filter-list-container">
-			<div className="filter-container">
-				<div className="filter-title-container">
-						<h4>REFINE SEARCH</h4>
+				<div className="filter-list-container">
+					<div className="filter-container">
+						<fieldset className="filters-fieldset">
+							<div>
+								<h4>FILTERS</h4>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="show_all" name="show_all" checked={this.state.show_all} onChange={event => this.showAll()} />
+								<label>Show All</label>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Water Sports</legend>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="surfing" name="surfing" checked={this.state.surfing} onChange={event => this.onInputChange(event.target.value, event.target.name)} />
+								<label>Surfing</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="kayaking" name="kayaking" checked={this.state.kayaking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.kayaking}/>
+								<label>Kayaking</label>
+							</div>
+						</fieldset>
+						<fieldset>
+						<legend>Mountain Sports</legend>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="snowboarding" name="snowboarding" checked={this.state.snowboarding} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.snowboarding}/>
+								<label>Snowboarding</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="skiing" name="skiing" checked={this.state.skiing} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.skiing}/>
+								<label>Skiing</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="mountain_biking" name="mountain_biking" checked={this.state.mountain_biking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.mountain_biking}/>
+								<label>Mountain Biking</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="rock_climbing" name="rock_climbing" checked={this.state.rock_climbing} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.rock_climbing}/>
+								<label>Rock Climbing</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="hiking" name="hiking" checked={this.state.hiking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.hiking}/>
+								<label>Hiking</label>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Other Sports</legend>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="skateboarding" name="skateboarding" checked={this.state.skateboarding} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.skateboarding}/>
+								<label>Skateboarding</label>
+							</div>
+						</fieldset>
+						<fieldset>
+							<legend>Certifications</legend>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="first_aid" name="first_aid" checked={this.state.first_aid} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.first_aid}/>
+								<label>First-Aid</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="cpr" name="cpr" checked={this.state.cpr} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.cpr}/>
+								<label>CPR</label>
+							</div>
+							<div className="checkbox-label-container">
+								<input type="checkbox" id="lifeguard" name="lifeguard" checked={this.state.lifeguard} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.lifeguard}/>
+								<label>Lifeguard</label>
+							</div>
+						</fieldset>
+					</div>
+				<NannyCards filtersSelected={this.state} nannyData={this.props.nannyData} />
 				</div>
-				<div className="show-all-checkbox">
-					<input type="checkbox" id="show_all" name="show_all" checked={this.state.show_all} onChange={event => this.showAll()} />
-					<label>Show All</label>
-				</div>
-
-				<hr className="line_1"></hr>    
-				<fieldset>
-					<legend>Water Sports</legend>
-					<div>
-						<input type="checkbox" id="surfing" name="surfing" checked={this.state.surfing} onChange={event => this.onInputChange(event.target.value, event.target.name)} />
-						<label>Surfing</label>
-					</div>
-					<div>
-						<input type="checkbox" id="kayaking" name="kayaking" checked={this.state.kayaking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.kayaking}/>
-						<label>Kayaking</label>
-					</div>
-				</fieldset>
-				<fieldset>
-				<legend>Mountain Sports</legend>
-					<div>
-						<input type="checkbox" id="snowboarding" name="snowboarding" checked={this.state.snowboarding} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.snowboarding}/>
-						<label>Snowboarding</label>
-					</div>
-					<div>
-						<input type="checkbox" id="skiing" name="skiing" checked={this.state.skiing} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.skiing}/>
-						<label>Skiing</label>
-					</div>
-					<div>
-						<input type="checkbox" id="mountain_biking" name="mountain_biking" checked={this.state.mountain_biking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.mountain_biking}/>
-						<label>Mountain Biking</label>
-					</div>
-					<div>
-						<input type="checkbox" id="rock_climbing" name="rock_climbing" checked={this.state.rock_climbing} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.rock_climbing}/>
-						<label>Rock Climbing</label>
-					</div>
-					<div>
-						<input type="checkbox" id="hiking" name="hiking" checked={this.state.hiking} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.hiking}/>
-						<label>Hiking</label>
-					</div>
-				</fieldset>
-				<fieldset>
-					<legend>Other</legend>
-					<div>
-						<input type="checkbox" id="skateboarding" name="skateboarding" checked={this.state.skateboarding} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.skateboarding}/>
-						<label>Skateboarding</label>
-					</div>
-				</fieldset>
-				<br></br>
-				<hr className="line_2"></hr>
-				<fieldset>
-					<legend>Certifications</legend>
-					<div>
-						<input type="checkbox" id="first_aid" name="first_aid" checked={this.state.first_aid} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.first_aid}/>
-						<label>First-Aid</label>
-					</div>
-					<div>
-						<input type="checkbox" id="cpr" name="cpr" checked={this.state.cpr} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.cpr}/>
-						<label>CPR</label>
-					</div>
-					<div>
-						<input type="checkbox" id="lifeguard" name="lifeguard" checked={this.state.lifeguard} onChange={event => this.onInputChange(event.target.value, event.target.name)} value={this.state.lifeguard}/>
-						<label>Lifeguard</label>
-					</div>
-				</fieldset>
 			</div>
-			<NannyCards filtersSelected={this.state} nannyData={this.props.nannyData} />
-		</div>
-		</div>
 		)
 	}
 
